cab86831d61053058877bc9539605979f46b4911
Robed <robedbeauvil@gmail.com>
2020-09-19 00:46:39 -0400

    added tests to quicksort alg

--- Javascript/divide-conquer/assignment3/quicksort.js
+++ Javascript/divide-conquer/assignment3/quicksort.js
@@ -1,38 +1,45 @@
-function quickSort(array, arrayLength) {
+function quickSort(inputArray, arrayLength) {
   if (arrayLength === 1) {
-      return array;
+      return inputArray;
   }
 
-  var pivot = choosePivot(array);
-  var partition = partitionAroundArray(array, pivot, arrayLength); 
+  var pivot = choosePivot(inputArray);
+  var partition = partitionAroundArray(inputArray, pivot); 
   var partition1 = partition[0];
   var partition2 = partition[1];
-  
+
   var firstPart = quickSort(partition1, partition1.length);
   var secondPart = quickSort(partition2, partition2.length);
 
   return firstPart.concat(secondPart);
 }
 
-function choosePivot(array) {
-  return array[0];
+function choosePivot(inputArray) {
+  return inputArray[0];
 }
 
-function partitionAroundArray(array, pivot, arrayLength) {
-  var i = 0;
-  for (var j = 1; j < arrayLength; j++) {
-      if (array[j] < pivot) {
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
+function partitionAroundArray(inputArray, pivot) {
+  if (inputArray.length === 0) {
+      return
+  }
+  var i = 1;
+  for (var j = 1; j < inputArray.length; j++) {
+      if (inputArray[j] < pivot) {
+          var temp = inputArray[i];
+          inputArray[i] = inputArray[j];
+          inputArray[j] = temp;
           i++;
       }
   }
-  var temp = array[i-1];
-  array[i-1] = array[0];
-  array[0] = temp;
-
-  return [array.slice(0,i), array.slice(i)];
+  var temp2 = inputArray[i-1];
+  inputArray[i-1] = inputArray[0];
+  inputArray[0] = temp2;
+ 
+  // if input array is already sorted let i be 2 so the array is slice properly
+  if (i === 1) {
+      i = 2;
+  }
+  return [inputArray.slice(0, i-1), inputArray.slice(i-1)];
 }
 
 module.exports = quickSort;
--- Javascript/test/quicksort.js
+++ Javascript/test/quicksort.js
@@ -13,3 +13,30 @@ QUnit.test('test array of length 2', assert => {
     var actual = quickSort([2, 1], 2);
   assert.deepEqual(actual, expected, 'should return [1, 2]');
 });
+
+QUnit.test('test array of length 3', assert => {
+    var expected = [1, 2, 3];
+    var actual = quickSort([2, 1, 3], 3);
+  assert.deepEqual(actual, expected, 'should return [1, 2, 3]');
+});
+
+QUnit.test('test array of even length', assert => {
+    var expected = [1, 2, 3, 4, 5, 6];
+    var actual = quickSort([2, 5, 3, 6, 1, 4], 6);
+  assert.deepEqual(actual, expected, 'should return [1, 2, 3, 4, 5, 6]')
+});
+
+QUnit.test('test array to help find issue', assert => {
+    var expected = [45, 49, 26, 22, 87, 65, 72, 41, 25];
+    expected = expected.sort(function(a, b){return a - b});
+    var actual = quickSort([45, 49, 26, 22, 87, 65, 72, 41, 25], 9);
+  assert.deepEqual(actual, expected, '22 and 25 should be in the proper location')
+});
+
+
+QUnit.test('test array of odd length', assert => {
+    var expected = [45, 49, 26, 22, 87, 65, 72, 41, 25, 1, 59, 51, 69, 3, 47, 16, 93, 96, 32, 92, 83, 43, 86, 60, 75, 89, 39, 74, 81, 50, 77, 42, 97, 6, 62, 63, 36, 29, 52, 5, 14, 33, 55, 27, 57, 2, 82, 48, 40, 95, 7, 100, 15];
+    expected = expected.sort(function(a, b){return a - b});
+    var actual = quickSort([45, 49, 26, 22, 87, 65, 72, 41, 25, 1, 59, 51, 69, 3, 47, 16, 93, 96, 32, 92, 83, 43, 86, 60, 75, 89, 39, 74, 81, 50, 77, 42, 97, 6, 62, 63, 36, 29, 52, 5, 14, 33, 55, 27, 57, 2, 82, 48, 40, 95, 7, 100, 15], 53);
+  assert.deepEqual(actual, expected, 'should return sorted array')
+});
