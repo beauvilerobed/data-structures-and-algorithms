907212daf2b5325930a036a7847d1389472e7938
Robed <robedbeauvil@gmail.com>
2020-09-20 19:31:52 -0400

    add test case for assignment and remove file from con.py

--- con.py
+++ con.py
@@ -1,10 +1,10 @@
 # first get all lines from file
-with open('numbers.txt', 'r') as f:
+with open('', 'r') as f:
     lines = f.readlines()
 
 # remove spaces
 lines = [line.replace('\n', ', ') for line in lines]
 
 # finally, write lines in the file
-with open('file.txt', 'w') as f:
+with open('', 'w') as f:
     f.writelines(lines)
