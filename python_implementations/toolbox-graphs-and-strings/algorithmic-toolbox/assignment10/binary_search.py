a4ecc3be5866df278a48ee45ee7ea4fe4734d31c
Robed <robedbeauvil@gmail.com>
2020-11-03 21:24:44 -0500

    removed aside in readme

--- python_implementations/toolbox-graphs-and-strings/algorithmic-toolbox/assignment10/binary_search.py
+++ python_implementations/toolbox-graphs-and-strings/algorithmic-toolbox/assignment10/binary_search.py
@@ -9,18 +9,22 @@ def binary_search_naive(keys, query):
 
 
 def binary_search(keys, query):
-    low, high = 0, len(keys)
-    while low + 1 < high:
-        mid = (low + high) // 2
-        if keys[mid] <= query:
-            low = mid
-        else:
-            high = mid
-    
-    if keys[low] != query:
-        return -1
-    else:
-        return low
+	start = 0
+	end = len(keys) - 1
+	
+	mid = ((end - start) //  2) + start
+	while start <= end:
+		if keys[mid] == query:
+			return mid
+		elif keys[mid] > query:
+			end = mid - 1 
+		elif keys[mid] < query:
+			start = mid + 1
+
+		mid = ((end - start) //  2) + start
+		
+	return -1
+
 
 def main():
     input_keys = list(map(int, input().split()))[1:]
--- python_implementations/toolbox-graphs-and-strings/algorithmic-toolbox/assignment11/README.md
+++ python_implementations/toolbox-graphs-and-strings/algorithmic-toolbox/assignment11/README.md
@@ -5,5 +5,3 @@
 **Output.** 1, if there is an element 
 that is repeated more than n / 2 times, 
 and 0 otherwise.
-
-As you might have already guessed, this problem can be solved by the divide-and-conquer algorithm in time <em>O(n log n)</em>. Indeed, if a sequence of length n contains a majority element, then the same element is also a majority element for one of its halves. Thus, to solve this problem you first split a given sequence into halves and recursively solve it for each half. Do you see how to combine the results of two recursive calls?
