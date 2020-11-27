451744c839e0f808dc71c906a1f35ce2b90ba307
Robed <robedbeauvil@gmail.com>
2020-11-27 14:27:59 -0500

    reduced parameters and return values

--- graph-search-shortest-paths/assignment3/file_reader.py
+++ graph-search-shortest-paths/assignment3/file_reader.py
@@ -4,46 +4,37 @@ import glob
 
 def generate_files(path='/tests/*'):
     file_path = os.getcwd() + path
-    file_path_length = len(file_path) - 1
+    len_file = len(file_path) - 1
 
     paths = glob.glob(file_path)
     input_files = []
     output_files = []
-    assignment = ''
 
     for path in paths:
-        if path[file_path_length: file_path_length + 5] == 'input':
+        if path[len_file: len_file + 5] == 'input':
             input_files.append(path)
-        elif path[file_path_length: file_path_length + 6] == 'output':
+        elif path[len_file: len_file + 6] == 'output':
             output_files.append(path)
-        else:
-            assignment = path
 
     input_files.sort()
     output_files.sort()
 
-    return input_files, output_files, assignment, file_path_length
+    return input_files, output_files
 
 
-def generate_inputs_outputs(input_files, output_files,
-                            assignment, file_path_length):
+def generate_cases(input_files, output_files):
     inputs_outputs = []
     for name1, name2 in zip(input_files, output_files):
-        if name1[file_path_length+5:] != name2[file_path_length+6:]:
-            print("input file", name1[file_path_length+5:],
-                  "is not the same as output file", name2[file_path_length+6:])
-            break
-        else:
-            case = []
-            with open(name1, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    case.append(int(line))
+        case = []
+        with open(name1, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                case.append(int(line))
 
-            with open(name2, 'r') as f:
-                line = f.readline()
-                data = int(line)
+        with open(name2, 'r') as f:
+            line = f.readline()
+            data = int(line)
 
-            inputs_outputs.append([case, data])
+        inputs_outputs.append([case, data])
 
     return inputs_outputs
--- graph-search-shortest-paths/assignment3/tests.py
+++ graph-search-shortest-paths/assignment3/tests.py
@@ -1,20 +1,16 @@
 import unittest
 from median_heap import median_heap
-from file_reader import generate_files, generate_inputs_outputs
+from file_reader import generate_files, generate_cases
 
 
-input_files, output_files, assignment, file_path_length = generate_files()
-inputs_outputs = generate_inputs_outputs(input_files, output_files,
-                                         assignment, file_path_length)
+input_files, output_files = generate_files()
+inputs_outputs = generate_cases(input_files, output_files)
 
 
 class TestMedianHeap(unittest.TestCase):
     def test_cases(self):
-        count = 1
         for input_value, output_value in inputs_outputs:
             self.assertEqual(median_heap(input_value), output_value)
-            print("passed test case:", count)
-            count += 1
 
 
 if __name__ == '__main__':
