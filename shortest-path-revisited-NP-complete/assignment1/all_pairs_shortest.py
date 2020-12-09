884eadb66031ebf88a9c20a5034d9276a9cd0722
Robed <robedbeauvil@gmail.com>
2020-12-08 21:38:01 -0500

    added floyd w.

--- shortest-path-revisited-NP-complete/assignment1/all_pairs_shortest.py
+++ shortest-path-revisited-NP-complete/assignment1/all_pairs_shortest.py
@@ -1,10 +1,49 @@
 # python3
 
 from file_reader import read_file
-from 
+import numpy as np
+import os
+import glob
 
-Edge = namedtuple('Edge', 'edge value')
 
-def bellman_ford(graph, source, n):
+def floyd_warshal(graph, n):
+    A = np.full((n+1, n+1, n+1), float('inf'))
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                A[i, j, 0] = 0
+            if (i, j) in graph:
+                A[i, j, 0] = graph[i, j]
+            else:
+                A[i, j, 0] = float('inf')
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                A[i, j, k] = min(A[i, j, k-1], A[i, k-1, k-1] + A[k, j, k-1])
 
-    A = {}
+    for i in range(1, n+1):
+        if A[i,i,n] < 0:
+            return 'Null'
+
+    minimum = np.min(A[:,:,n])
+    return minimum
+
+
+def main():
+    file_path = os.getcwd() + '/assignments/*'
+    paths = glob.glob(file_path)
+
+    graphs = []
+    for path in paths:
+        graph_data = read_file(path)
+        graphs.append(graph_data)
+
+    for graph_data in graphs:
+        graph = graph_data[0]
+        n = graph_data[1]
+        print(floyd_warshal(graph, n))
+
+
+if __name__ == '__main__':
+    main()
--- shortest-path-revisited-NP-complete/assignment1/file_reader.py
+++ shortest-path-revisited-NP-complete/assignment1/file_reader.py
@@ -1,17 +1,16 @@
-from collections import namedtuple
-
-Edge = namedtuple('Edge', 'edge value')
+from collections import namedtuple, defaultdict
 
 
 def read_file(file):
-    graph = {}
+    graph = defaultdict(list)
     with open(file) as f:
         lines = f.readlines()
         n = int(lines[0].split()[0])
         for line in lines[1:]:
             data = list(map(int, line.split()))
             vertex = data[0]
-            edge = Edge(data[1], data[2])
-            graph[vertex] = edge
+            edge = data[1]
+            edge_len = data[2]
+            graph[vertex, edge] = edge_len
 
         return graph, n
