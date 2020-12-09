2c45bb91f86f03a43e67f5e57d6ede7c622edcc2
Robed <robedbeauvil@gmail.com>
2020-12-08 23:21:43 -0500

    Update file reader

--- shortest-path-revisited-NP-complete/assignment1/file_reader.py
+++ shortest-path-revisited-NP-complete/assignment1/file_reader.py
@@ -1,16 +1,13 @@
-from collections import namedtuple, defaultdict
+# python3 
 
 
 def read_file(file):
-    graph = defaultdict(list)
+    graph = {}
     with open(file) as f:
         lines = f.readlines()
         n = int(lines[0].split()[0])
         for line in lines[1:]:
-            data = list(map(int, line.split()))
-            vertex = data[0]-1
-            edge = data[1]-1
-            edge_len = data[2]
-            graph[vertex, edge] = edge_len
+            vertex, edge, edge_len = map(int, line.split())
+            graph[vertex-1, edge-1] = edge_len
 
         return graph, n
